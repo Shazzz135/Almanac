a722816a36e656c56e1a27c528e9b2862288a741
Shazzz <nickshahbaz135@gmail.com>
2026-02-05 15:39:01 -0500

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,14 +9,15 @@ import Board from "./pages/Board"
 //Provider Imports
 import { AuthProvider } from './provider/AuthProvider'
 import Navbar from './components/ui/Navbar'
+import Footer from './components/ui/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-gradient-to-t from-gray-900 to-gray-950">
+    <div className="min-h-screen w-full bg-gradient-to-t from-gray-900 to-gray-950 flex flex-col">
       <AuthProvider>
         <Router>
           <Navbar />
-          <div className="flex flex-col items-center justify-center pt-16">
+          <div className="flex-1 flex flex-col items-center justify-center pt-16">
             <Routes>
               {/* Main Pages */}
               <Route path="/" element={<Landing />} />
@@ -31,6 +32,7 @@ function App() {
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </div>
+          <Footer />
         </Router>
       </AuthProvider>
     </div>
